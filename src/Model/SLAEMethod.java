3fa5041779c85e78adb777e65d850c3fe72e07f3
Demiani <demkafrog@gmail.com>
2021-06-05 09:19:23 +0300

    SlaeMethod methods double fields changed to BigDecimal, ChartSolve world expanding in case of too close location of inteseciton to maxWorldCoordinate

--- src/Model/SLAEMethod.java
+++ src/Model/SLAEMethod.java
@@ -1,100 +1,9 @@
 package Model;
 
-import Model.exception.InconsistentMatrixException;
-
-public abstract class SLAEMethod {
-    protected double[][] matrix;
-    protected double[] result;
-    protected int length;
-    protected int width;
+public class SLAEMethod {
     protected int sumCount = 0, substrCount = 0,multCount = 0, divCount = 0;
 
-    protected SLAEMethod() {}
-
-    protected SLAEMethod(double[][] matrix) {
-        this.matrix = matrix;
-        this.length = matrix.length;
-        this.width = matrix[0].length;
-        this.result = new double[length];
-    }
-
-//    protected abstract void makeTriangleView();
-
-    public double[] solve() {
-//        makeTriangleView();
-        int consistence = consistence();
-        if(consistence < 0)
-            throw new InconsistentMatrixException("Given matrix is inconsistent");
-        else if(consistence > 0){
-            return null;
-        }
-        backtrace();
-        return result;
-    }
-
-    protected void checkMatrix() {
-
-    }
-
-    private void backtrace() {
-        double sum;
-        for (int i = length-1; i >= 0; i--) {
-            sum = 0;
-            for (int j = i+1; j < length; j++) {
-                sum += matrix[i][j] * result[j];
-                multCount++;
-                sumCount++;
-            }
-            result[i] = (matrix[i][width-1] - sum)/matrix[i][i];
-            substrCount++;
-            divCount++;
-        }
-    }
-
-    private int consistence() {
-        int regularMatrixRank = getRegularMatrixRank();
-        int extendedMatrixRank = getExtendedMatrixRank();
-        return regularMatrixRank < extendedMatrixRank ? -1 : (regularMatrixRank == extendedMatrixRank ? 0 : 1);
-    }
-
-    private int getExtendedMatrixRank() {
-        int nonZeroRawCounter = 0;
-        for (int i = 0; i < length; i++) {
-            for (int j = 0; j < width; j++) {
-                if(matrix[i][j] != 0){
-                    nonZeroRawCounter++;
-                    j = width;
-                }
-            }
-        }
-        return nonZeroRawCounter;
-    }
-
-    private int getRegularMatrixRank() {
-        int nonZeroRawCounter = 0;
-        for (int i = 0; i < length; i++) {
-            for (int j = 0; j < width-1; j++) {
-                if(matrix[i][j] != 0){
-                    nonZeroRawCounter++;
-                    j = width;
-                }
-            }
-        }
-        return nonZeroRawCounter;
-    }
-
-    public double[][] getMatrix() {
-        return matrix;
-    }
-
-    public void setMatrix(double[][] matrix) {
-        nullifyCounters();
-        this.matrix = matrix;
-        this.length = matrix.length;
-        this.width = matrix[0].length;
-    }
-
-    void nullifyCounters() {
+    protected void nullifyCounters() {
         sumCount = 0;
         substrCount = 0;
         multCount = 0;
@@ -116,4 +25,5 @@ public abstract class SLAEMethod {
     public int getDivCount() {
         return divCount;
     }
+
 }
